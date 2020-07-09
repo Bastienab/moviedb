8a3cc8fde460aae31f32cfdebf1923782801be2b
Bastien Alessandri <bastienalessandri@gmail.com>
2020-07-09 15:12:02 +0200

    modif in contact page

--- src/app/context/contact/Contact.jsx
+++ src/app/context/contact/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <div>
       <div style={{width: "100%", height:"100%", display: "flex", justifyContent: "center", alignItems: "center", marginTop: "100px"}}>
-          <h3>Contact page under construction...</h3>
+          <p>Contact page under construction...</p>
       </div>
     </div>
       
